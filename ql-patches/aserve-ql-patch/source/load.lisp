14cc446f61a7ee8039a8daae767f619cfef8238f
Dave Cooper <open-source@genworks.com>
2012-04-01 21:53:51 -0400

    removed no longer needed quicklisp patches and quicklisp-local directory

--- ql-patches/aserve-ql-patch/source/load.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :cl-user)
-
-(when (com.genworks.lisp:featurep :allegro)
-  (load
-   (merge-pathnames
-    "quicklisp-local/portableaserve-20110730-cvs/aserve/aserve.asd-dummy"
-    com.genworks.lisp:*genworks-source-home*)))
