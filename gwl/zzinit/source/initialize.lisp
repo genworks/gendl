2f43f12ba6801f755287c9014d35777223bdba6c
Dave Cooper <david.cooper@genworks.com>
2017-06-03 22:59:02 -0400

    hacked CCL to avoid overflow of sleep value

--- gwl/zzinit/source/initialize.lisp
+++ gwl/zzinit/source/initialize.lisp
@@ -206,3 +206,23 @@ Perhaps a zombie process is holding port ~a?~%" port port))
     (start-gwl) 
     anything-changed?))
 
+
+
+#+ccl
+(in-package :ccl)
+#+ccl
+(let (*warn-if-redefine-kernel*)
+  (defun %windows-sleep (millis)
+
+    (dotimes (n 3)
+      (unless (typep millis '(unsigned-byte 32))
+	(setq millis (/ millis 100))))
+    
+    (do* ((start (floor (get-internal-real-time)
+			(floor internal-time-units-per-second 1000))
+		 (floor (get-internal-real-time)
+			(floor internal-time-units-per-second 1000)))
+	  (millis millis (- stop start))
+	  (stop (+ start millis)))
+	 ((or (<= millis 0)
+	      (not (eql (#_SleepEx millis #$true) #$WAIT_IO_COMPLETION)))))))
