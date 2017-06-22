8ae1bd985772f72a5ce2c28f5cb40656fcc9c3da
Dave Cooper <david.cooper@genworks.com>
2017-06-22 18:09:35 -0400

    moved ccl low-level code out of initialize.lisp because of non portable reader directive -- actually should go to glisp.

--- gwl/zzinit/source/initialize.lisp
+++ gwl/zzinit/source/initialize.lisp
@@ -212,38 +212,12 @@ Perhaps a zombie process is holding port ~a?~%" port port))
 
 
 
-#+ccl
-(in-package :ccl)
-
 ;;
 ;; FLAG -- get platform-specific stuff into glisp package. 
 ;;
 
-#+nil
-(defun %windows-sleep (millis)
-  (do* ((start (floor (get-internal-real-time)
-                      (floor internal-time-units-per-second 1000))
-               (floor (get-internal-real-time)
-                      (floor internal-time-units-per-second 1000)))
-        (millis millis (- stop start))
-        (stop (+ start millis)))
-       ((or (<= millis 0)
-            (not (eql (#_SleepEx millis #$true) #$WAIT_IO_COMPLETION))))))
-
-
 #+(and ccl windows-target)
-(let (*warn-if-redefine-kernel*)
-  (defun %windows-sleep (millis)
+(load (compile-file (merge-pathnames "fix-windows-ccl-sleep.lisp" *load-truename*)))
 
-    (dotimes (n 3)
-      (unless (typep millis '(unsigned-byte 32))
-	(setq millis (/ millis 100))))
-    
-    (do* ((start (floor (get-internal-real-time)
-			(floor internal-time-units-per-second 1000))
-		 (floor (get-internal-real-time)
-			(floor internal-time-units-per-second 1000)))
-	  (millis millis (- stop start))
-	  (stop (+ start millis)))
-	 ((or (<= millis 0)
-            (not (eql (#_SleepEx millis #$true) #$WAIT_IO_COMPLETION)))))))
+
+
